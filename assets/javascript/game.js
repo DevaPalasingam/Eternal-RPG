efb7c81e52b03744fe4cbf713773aa50a1f5595f
Deva <deva.palasingam@gmail.com>
2018-10-22 18:46:45 -0500

    name

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,21 @@
-document.write("Hello World!");
+var hasName = false;
+
+var stats = {
+	name: "",
+	health: 1,
+	attack: 1,
+	mana: 1
+};
+
+document.onkeyup = function (e) {
+	e = e || window.event;
+
+	if (e.key != "Enter" && !hasName) {
+		stats.name = stats.name + e.key;
+		document.getElementById("keystroke").innerHTML = stats.name;
+	}
+	else {
+		hasName = true;
+		document.getElementById("name").innerHTML = stats.name;
+	}
+}
